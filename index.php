cbf8c46aece0af2beb05bc48e598959c5c5e39ea
gunawanzakiahhalim <43396498+gunawanzakiahhalim@users.noreply.github.com>
2020-02-20 20:07:05 +0700

    updload layout to github
    uload layout mywebsite

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Layout</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="container">
+<!-- containter --> 
+<!-- header -->
+<div class="header cf ">
+
+<div class="header_judul ">
+<h1>Header</h1>
+</div>
+
+    <div class="header_list">
+    <a href>Home</a>
+    <a href>Logout</a>
+    </div>
+</div>
+<!-- header -->
+
+<!-- tengah -->
+<div class="tengah cf ">
+<!-- navigasi -->
+<div class="navigasi ">
+<div class="menu_judul">
+<h1>Navigasi</h1>  
+</div>
+<div class="menu_list">
+<ul>
+    <li><a href="">Kategori 1</a><li>
+    <li><a href="">Kategori 2</a><li>
+    <li><a href="">Kategori 3</a><li>
+    <li><a href="">Kategori 4</a><li>
+    <li><a href="">Kategori 1</a><li>
+    <li><a href="">Kategori 2</a><li>
+    <li><a href="">Kategori 3</a><li>
+    <li><a href="">Kategori 4</a><li>
+    <li><a href="">Kategori 1</a><li>
+    <li><a href="">Kategori 2</a><li>
+    <li><a href="">Kategori 3</a><li>
+    <li><a href="">Kategori 4</a><li>
+    <li><a href="">Kategori 1</a><li>
+    <li><a href="">Kategori 2</a><li>
+    <li><a href="">Kategori 3</a><li>
+    <li><a href="">Kategori 4</a><li>
+    <li><a href="">Kategori 1</a><li>
+    <li><a href="">Kategori 2</a><li>
+    <li><a href="">Kategori 3</a><li>
+    <li><a href="">Kategori 4</a><li>
+    <li><a href="">Kategori 1</a><li>
+    <li><a href="">Kategori 2</a><li>
+    <li><a href="">Kategori 3</a><li>
+    <li><a href="">Kategori 4</a><li>
+    <li><a href="">Kategori 1</a><li>
+    <li><a href="">Kategori 2</a><li>
+    <li><a href="">Kategori 3</a><li>
+    <li><a href="">Kategori 4</a><li>
+    <li><a href="">Kategori 1</a><li>
+    <li><a href="">Kategori 2</a><li>
+    <li><a href="">Kategori 3</a><li>
+    <li><a href="">Kategori 4</a><li>
+    <li><a href="">Kategori 4</a><li>
+    <li><a href="">Kategori 1</a><li>
+    <li><a href="">Kategori 2</a><li>
+    <li><a href="">Kategori 3</a><li>
+    <li><a href="">Kategori 4</a><li>
+    <li><a href="">Kategori 1</a><li>
+    <li><a href="">Kategori 2</a><li>
+    <li><a href="">Kategori 3</a><li>
+    <li><a href="">Kategori 4</a><li>
+    <li><a href="">Kategori 4</a><li>
+    <li><a href="">Kategori 1</a><li>
+    <li><a href="">Kategori 2</a><li>
+    <li><a href="">Kategori 3</a><li>
+    <li><a href="">Kategori 4</a><li>
+</ul>
+</div>
+
+</div>
+<!-- navigasi -->
+<!-- konten -->
+<div class="konten">
+<!-- konten area  -->
+<div class="area">
+
+<div class="konten_judul">
+<h1>konten</h1>
+</div>
+
+<!-- Konten List -->
+<div class="konten_list">
+<div class="konten_list_navigasi cf">
+<a href="">Navigasi 1</a>
+<a href="">Navigasi 1</a>
+</div>
+<div class="konten_list_judul">
+<h1>Judul List</h1>
+</div>
+
+<div class="konten_list_konten cf">
+
+<!-- Kolom konten_list_konten -->
+<div class="kolom">
+    <!-- Box -->
+    <div class="box">
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    </div>
+    <div class="box">
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    </div>
+    <div class="box">
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    </div>
+    <div class="box">
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    </div>
+    <!-- Box -->
+</div>
+<!-- Kolom konten_list_konten -->
+
+<!-- Kolom konten_list_konten -->
+<div class="kolom">
+    <!-- Box -->
+    <div class="box">
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    </div>
+    <div class="box">
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    </div>
+    <div class="box">
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    </div>
+    <div class="box">
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    </div>
+    <!-- Box -->
+</div>
+<!-- Kolom konten_list_konten -->
+
+</div>
+</div>
+<!-- Konten List -->
+
+</div>
+<!-- konten area  -->
+</div>
+<!-- konten -->
+</div>
+<!-- tengah -->
+
+<!-- footer -->
+<div class="footer">
+<h1>footer</h1>
+</div>
+<!-- konten -->
+
+<!-- containter --> 
+</div>
+</body>
+</html>
